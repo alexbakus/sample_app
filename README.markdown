4c1426a44e3d2363e8c717b02bb864d2db917ff0
Alex Bakus <alex@finask.ca>
2012-06-30 22:55:34 -0400

    Initial Commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: sample app - third installment
+
+This is the second application for [*Ottawa Financial Advisers*](http://www.finask.ca/ottawa/) by [Ottawa Business Developer](http://www.alexanderbakus.com/) - Alex Bakus.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'f75b4cc533cbda0977d410736ba16b539f0fd5b9303b3c1c077b7f0b219a87d45d05a860e1d9077d4081b6b1b4bd503f821c0e4016536782a7acb6075fa9673c'
